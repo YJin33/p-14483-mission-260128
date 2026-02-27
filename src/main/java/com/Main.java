f1b9cb35fc2d76d7403ec86a5f68a266fdf443d1
YJin33 <ujin3261@gmail.com>
2026-02-27 19:34:06 +0900

    사칙연산-2ê°인ì-인수2개까지(t7)

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,6 +1,22 @@
 package com;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        try{
+            String s = "";
+            while(!s.equals("exit")){
+                s = br.readLine();
+                Calc.run(s);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
